c329976f1c24f3dc73eaf8572184b3cf1017a107
tomg <tom.germeau@gmail.com>
2020-04-08 23:08:27 -0400

    add eu data

--- update.sh
+++ update.sh
@@ -1,1 +1,2 @@
 curl "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv" > us-states.csv
+curl 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/' > eu-states.csv
