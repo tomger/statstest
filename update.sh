068188d95129d0706298b5d77fe33b5a6d10ae93
tomg <tom.germeau@gmail.com>
2020-06-10 15:42:54 -0400

    ksjngsnggs

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 curl "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv" > us-states.csv
 curl 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/' > eu-states.csv
+curl 'https://covidtracking.com/api/v1/states/daily.csv' > us-testing.csv
