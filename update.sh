6cf6683a5998ab2803a1523a69da66d958d41289
Tom Ger <tom.germeau@gmail.com>
2020-07-04 11:57:04 -0400

    source

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 curl "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv" > us-states.csv
 curl 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/' > eu-states.csv
 curl 'https://covidtracking.com/api/v1/states/daily.csv' > us-testing.csv
-date > lastupdate
+echo "date" > lastupdate
+date >> lastupdate
