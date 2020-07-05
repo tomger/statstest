cf68879f953391499480798af5c54abbe831b78b
Tom Ger <tom.germeau@gmail.com>
2020-07-05 16:33:29 -0400

    leaner

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-curl "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv" > us-states.csv
-curl 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/' | awk -F "/" 'NR==1 || $2 >= 5' > eu-states.csv # data newer that 5th month
+# data newer that 5th month
+curl "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv" | awk -F "-" 'NR==1 || $2 >= 5' > us-states.csv
+curl 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/' | awk -F "/" 'NR==1 || $2 >= 5' | cut -d, -f2-7,9-10 > eu-states.csv
 curl 'https://covidtracking.com/api/v1/states/daily.csv' > us-testing.csv
 echo "date" > lastupdate
 date >> lastupdate
