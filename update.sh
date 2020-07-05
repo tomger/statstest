6780f6262f3717e9204c65e47ab0a27e5bff9e87
Tom Ger <tom.germeau@gmail.com>
2020-07-05 16:00:14 -0400

    data reduce

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 curl "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv" > us-states.csv
-curl 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/' > eu-states.csv
+curl 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/' | awk -F "/" '$2 >= 5' > eu-states.csv # data newer that 5th month
 curl 'https://covidtracking.com/api/v1/states/daily.csv' > us-testing.csv
 echo "date" > lastupdate
 date >> lastupdate
