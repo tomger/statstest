c8b74d79af9a7668ff2c654b2dcab940d2630ac7
Tom Ger <tom.germeau@gmail.com>
2020-07-07 17:53:03 -0400

    r

--- update.sh
+++ update.sh
@@ -4,3 +4,7 @@ curl 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/' | awk -F "/
 curl 'https://covidtracking.com/api/v1/states/daily.csv' > us-testing.csv
 echo "date" > lastupdate
 date >> lastupdate
+
+# more data
+# https://www.cdc.gov/nhsn/covid19/report-overview.html
+# https://github.com/CSSEGISandData/COVID-19
