e467bd4172f598a37c382c71c95a15f535dfd783
Tom Ger <tom.germeau@gmail.com>
2020-09-07 20:30:02 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
+logger covid_update_start
+python get.py
+logger covid_update_end
 echo "date" > lastupdate
 date >> lastupdate
-python get.py
 
+#git fetch --all && git checkout --force "origin/master"
