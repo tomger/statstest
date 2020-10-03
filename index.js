cc7eba32f86f4a72a947de350f17d440f47c14c8
Tom Ger <tom.germeau@gmail.com>
2020-10-03 11:03:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -582,7 +582,6 @@ class DetailView extends HTMLElement {
         border: 1px solid var(--colorSeparator);
         display: flex;
         align-items: center;">
-        <svg style="margin-right: 8px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
         Following
     </div>`;
     let addButton = `<div 
